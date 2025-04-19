546122a44235947996b1c9f0883223211029698a
Shreeyog-Gaikwad <shreeyoggaikwad@gmail.com>
2025-04-19 13:43:05 +0530

    Contacts Code Added

--- app/Pages/send.tsx
+++ app/Pages/send.tsx
@@ -1,10 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, Button , StyleSheet} from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 const send = () => {
+
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+
+      if (data.length > 0) {
+        setContacts(data);
+      }
+    } else {
+      alert('Permission denied');
+    }
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+
   return (
-    <View>
-      <Text>send</Text>
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Phone Contacts:</Text>
+      <FlatList
+        data={contacts}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text style={{ marginVertical: 5 }}>
+            {item.name} {item.phoneNumbers?.[0]?.number}
+          </Text>
+        )}
+      />
     </View>
   )
 }
