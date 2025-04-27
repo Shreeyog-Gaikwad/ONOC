5faa7adcbe23ad184565f40450b28136d358499c
Srushtiii13 <srushtis1314@gmail.com>
2025-04-27 15:19:40 +0530

    upload page

--- app/Pages/upload.jsx
+++ app/Pages/upload.jsx
@@ -205,7 +205,7 @@ const styles = StyleSheet.create({
     position: 'relative',
   },
   container: {
-    paddingTop: 90,
+    paddingTop: 100,
     height: "104%",
     width : "100%",
     backgroundColor: "white",
