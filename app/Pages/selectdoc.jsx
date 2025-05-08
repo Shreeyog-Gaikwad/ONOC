8927f821ba019e6be6064b87c581a444f5aedc72
Shreeyog-Gaikwad <shreeyoggaikwad@gmail.com>
2025-05-08 10:19:49 +0530

    Other pload some changes done

--- app/Pages/selectdoc.jsx
+++ app/Pages/selectdoc.jsx
@@ -8,64 +8,35 @@ import {
   MaterialCommunityIcons,
   FontAwesome5,
 } from "@expo/vector-icons";
+import { ScrollView } from "react-native-gesture-handler";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const selectdoc = () => {
   const { ...item } = useLocalSearchParams();
 
   const [selectedDocs, setSelectedDocs] = useState([]);
 
-  const documents = [
-    { name: "Aadhar Card", lib: Ionicons, iconName: "finger-print", size: 36 },
-    { name: "PAN Card", lib: FontAwesome, iconName: "id-card", size: 32 },
-    { name: "Driving License", lib: MaterialIcons, iconName: "drive-eta", size: 38 },
-    { name: "Voter ID", lib: MaterialCommunityIcons, iconName: "vote-outline", size: 32 },
-    { name: "Passport", lib: FontAwesome5, iconName: "passport", size: 32 },
-    { name: "Ration Card", lib: MaterialCommunityIcons, iconName: "food-variant", size: 34 },
-    { name: "Birth Certificate", lib: Ionicons, iconName: "document-text-outline", size: 30 },
-    { name: "SSC Marksheet", lib: MaterialCommunityIcons, iconName: "certificate-outline", size: 34 },
-    { name: "HSC Marksheet", lib: MaterialCommunityIcons, iconName: "school-outline", size: 36 },
-    { name: "Domicile Certificate", lib: Ionicons, iconName: "home-outline", size: 34 },
-    { name: "Caste Certificate", lib: MaterialCommunityIcons, iconName: "badge-account-outline", size: 36 },
-    { name: "Other", lib: MaterialIcons, iconName: "folder", size: 34, multiUpload: true },
-  ];
-
-  const handlePress = (index) => {
-    if (selectedDocs.includes(index)) {
-      setSelectedDocs(selectedDocs.filter((i) => i !== index));
-    } else {
-      setSelectedDocs([...selectedDocs, index]);
-    }    
-  };
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.head1}>Select Documents</Text>
-        <Text style={styles.head2}>
-          to send <Text style={styles.name}>{item.name} !</Text>
-        </Text>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.head1}>Select Documents</Text>
+          <Text style={styles.head2}>
+            to send <Text style={styles.name}>{item.name} !</Text>
+          </Text>
+        </View>
+
+        <ScrollView contentContainerStyle={styles.boxContainer}>
+
+        </ScrollView>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btntxt}>Send Documents</Text>
+        </TouchableOpacity>
+
       </View>
-
-      <View style={styles.boxContainer}>
-        {documents.map((doc, index) => {
-          const isSelected = selectedDocs.includes(index);
-          const iconColor = isSelected ? "green" : "black";
-          const borderColor = isSelected ? "green" : "black";
-          const Icon = doc.lib;
-          return (
-            <TouchableOpacity key={index} style={[styles.box, { borderColor: borderColor }]} onPress={()=>handlePress(index)}>
-              <Icon name={doc.iconName} size={doc.size} color={iconColor} />
-              <Text style={styles.docText}>{doc.name}</Text>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-
-      <TouchableOpacity style={styles.btn}>
-        <Text style={styles.btntxt}>Send Documents</Text>
-      </TouchableOpacity>
-
-    </View>
+    </GestureHandlerRootView>
   );
 };
 
@@ -73,6 +44,7 @@ export default selectdoc;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     margin: 20,
     marginTop: 50,
     height: '100%',
@@ -90,10 +62,8 @@ const styles = StyleSheet.create({
   },
   boxContainer: {
     marginTop: 20,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    alignItems: "center",
-    justifyContent: "center",
+    height: '85%',
+    backgroundColor: 'grey'
   },
   box: {
     height: 90,
